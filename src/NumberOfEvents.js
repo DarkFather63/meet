bf85d9068b3bae3605d4d8290459d00749420f20
DarkFather63 <erynlcraig@gmail.com>
2022-07-22 10:53:31 -0700

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
-import { ErrorAlert } from './Alert';
 
 class NumberofEvents extends Component {
 
 
   handleInputChanged = (event) => {
     const selectedValue = parseInt(event.target.value);
-    if (selectedValue > 20 || selectedValue < 0) {
-      return this.setState({
-        numberOfEvents: 20,
-        errorText: 'Please select a number between 0 and 20.'
+    if (selectedValue <= 20 && selectedValue > 0) {
+      this.setState({
+        numberOfEvents: selectedValue
       });
-    } else if (selectedValue <= 20 && selectedValue > 0) {
-      return this.setState({
-        numberOfEvents: selectedValue,
-        errorText: ''
+    } else {
+      this.setState({
+        numberOfEvents: 20,
+        Error: 'Please select a number greater than 0.'
       })
     }
     this.props.updateEvents(undefined, selectedValue);
@@ -23,10 +21,8 @@ class NumberofEvents extends Component {
   render() {
     const { numberOfEvents } = this.props;
     return <div className="event-count">
-      <ErrorAlert text={this.state.errorText} />
       <p>Number of Events:</p>
-
-      <input className="event-number" type="number" max="100" value={numberOfEvents} onChange={this.handleInputChanged}>
+      <input className="event-number" type="number" min="1" max="100" value={numberOfEvents} onChange={this.handleInputChanged}>
 
       </input>
 
