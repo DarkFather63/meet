ad087e7725a861168566768568860e9ab151974e
DarkFather63 <erynlcraig@gmail.com>
2022-07-30 16:20:51 -0700

    fixing pie chart render

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -5,33 +5,34 @@ const EventGenre = ({ events }) => {
 
   const [data, setData] = useState([]);
 
-
-  const getData = () => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-    const data = genres.map((genre) => {
-      const value = events.filter((event) => event.summary.split('').includes(genre)).length;
-      return { name: genre, value: value };
-    });
-    return data;
-  };
-
   useEffect(() => {
     setData(() => getData());
   },
     [events]
   );
 
+  const getData = () => {
+    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
+    const data = genres.map((genre) => {
+      const value = events.filter((event) => event.summary.split(" ").includes(genre)).length;
+      return { name: genre, value };
+    });
+    return data;
+  };
+
+
+
   return (
-    <ResponsiveContainer height={400}>
-      <PieChart width={400} height={400}>
+    <ResponsiveContainer>
+      <PieChart width={500} height={400}>
         <Pie
           data={data}
-          cx={200}
-          cy={200}
+          cx={50}
+          cy={50}
           labelLine={false}
           outerRadius={80}
           fill='#fff'
-          dataKey='value'
+          dataKey="value"
           label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
         >
         </Pie>
